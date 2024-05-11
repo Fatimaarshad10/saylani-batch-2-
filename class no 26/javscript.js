66782d5857d3bc44debdca4267863a421199dea1
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-05-11 23:57:53 +0500

    class no 28

---
+++ class no 26/javscript.js
@@ -0,0 +1,68 @@
+// // // Write a JavaScript program to find the maximum number in an array. 
+
+// // function findMax(arr) {
+// //     if (arr.length === 0) {
+// //         return "Array is empty";
+// //     }
+
+// //     let max = arr[0];
+// //     for (let i = 1; i < arr.length; i++) {
+// //         if (arr[i] > max) {
+// //             max = arr[i];
+// //         }
+// //     }
+// //     return max;
+// // }
+
+// // // Example usage:
+// // const numbers = [5, 8, 2, 10, 3];
+// // console.log("Maximum number:", findMax(numbers));
+
+// // // Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards). 
+// // function isPalindrome(str) { 
+// //     return str === str.split("").reverse().join(""); 
+  
+// //   }
+
+// // //   Write a JavaScript function to check if a given number is prime. 
+// //   function isPrime(num) {
+// //     // Check if the number is less than 2
+// //     if (num < 2) {
+// //         return false;
+// //     }
+// //     // Check for divisibility by numbers from 2 to half of the number
+// //     for (let i = 2; i <= num / 2; i++) {
+// //         if (num % i === 0) {
+// //             return false;
+// //         }
+// //     }
+// //     return true;
+// // }
+
+// // // Example usage:
+// // const number = 17;
+// // console.log(number + " is prime:", isPrime(number));
+
+
+// <!-- const numbers = [1, 2, 3, 4, 5];
+// const evenNumbers = numbers.filter(num =>  num % 2 === 0);
+// console.log(evenNumbers); // Output: [2, 4]
+
+// const ages = [20, 25, 30, 35];
+// const isAdult = ages.every(age => age >= 18);
+// console.log(isAdult); // Output: true
+
+// const colors = ['red', 'green', 'blue'];
+// colors.forEach(color => console.log(color));
+
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// const slicedArray = numbers.slice(1, 3);
+// console.log(slicedArray); // Output: [2, 3]
+
+// const elements = ['Fire', 'Air', 'Water'];
+// console.log(elements.join()); // Output: "Fire,Air,Water"
+// console.log(elements.join(' - ')); // Output: "Fire - Air - Water" -->
