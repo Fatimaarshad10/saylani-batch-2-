c2b4fe4386422f217b505ac8e09a5192fc17e40c
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-05-19 19:16:22 +0500

    Class no 30 19-05-2024 -1

---
+++ class no 30/about/about.js
@@ -0,0 +1,54 @@
+// console.log('about.js')
+// let name = "about.js"
+// let age = 20
+
+// export {name , age}
+// export default name 
+
+// function First (name , age){
+//     console.log(name)
+//     console.log(age)
+// }
+
+// First('about.js' , 20)
+//  export {First}
+
+// let value = function (data){
+//     console.log('hello this is expression function' , data)
+// }
+// value('my vaue')
+// let Data = (name , age )=>{
+//     console.log(name)
+//     console.log(age)
+// }
+// Data('sjsjs' , 344)
+
+// let Chair = {
+//     color : 'red',
+//     price : 3000
+// }
+// let Chair2 = {
+//     color : 'purple',
+//     price : 3000,
+//     data (){
+//         console.log('hello')
+//     }
+// }
+// Chair2.data()
+// class House {
+//     constructor(name , town , street , rooms){
+//         this.name = name,
+//         this.town = town,
+//         this.street = street,
+//         this.rooms = rooms
+//     }
+// }
+
+// const houseNo1 = new House('saylaniHouse' , 'Faislabad' , '999' , 78)
+// console.log(houseNo1 )
+
+var name = 233
+var name = "fatima"
+
+
+export default name 
---
+++ class no 30/home/home.js
@@ -0,0 +1,7 @@
+// console.log('home.js')
+// import  {First}  from "../about/about.js";
+// First('amna' , 37)
+
+
+import name from "../about/about.js";
+console.log(name)
