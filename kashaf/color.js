f5ace11aeecc23fec3c35681cadc8e1604e9b44c
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-06-03 17:36:28 +0500

    class no 34

---
+++ kashaf/color.js
@@ -0,0 +1,13 @@
+let btn1=document.querySelector('btn1')
+let btn2=document.querySelector('btn2')
+let body=document.querySelector('body')
+btn1.addEventListener("click",()=>{
+    document.querySelector.addClassList=("body")
+    btn1.addEventListener.addClassList=("black");
+    btn1.addEventListener.removeClassList=("white");
+})
+btn2.addEventListener("click",()=>{
+    document.querySelector.addClassList=("body")
+    btn2.addEventListener.addClassList=("white");
+    btn2.addEventListener.removeClassList=("black");
+})
