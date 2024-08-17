b4fd861e4f18b5b27ba345a5efb578e53080a8d4
Fatima Arshad <fatimaarshad118@gmail.com>
2024-08-17 14:54:15 +0500

    class no 50

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,41 @@
 import Home from "./home";
 import Login from "./login";
 import Navbar from "./navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  // let content;
-  // let isLogin = false;
-  // if(isLogin){
-  //   content = <Home/>
-  // }else{
-  //   content = <Login/>
-  // }
   return (
     <>
-    <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+export default App;
+
+// let content;
+// let isLogin = false;
+// if(isLogin){
+//   content = <Home/>
+// }else{
+//   content = <Login/>
+// }
+
+{
+  /* <h1>Hello</h1>
+    <p>hello world</p>
+    <SignUp/> */
+}
+
+{
+  /* <Home/> */
+}
+{
+  /* <Navbar/>
       {true ? (
         <>
           <Home />
@@ -20,9 +44,5 @@ function App() {
         <>
           <Login />
         </>
-      )}
-    </>
-  );
+      )} */
 }
-
-export default App;
