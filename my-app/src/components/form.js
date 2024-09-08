f21059d3647997c41f1fb886ac237e5290f50287
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-09-08 10:58:13 -0700

    react hook form , yup

--- my-app/src/components/form.js
+++ my-app/src/components/form.js
@@ -1,62 +1,36 @@
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-
-// Step 1: Create a validation schema with yup
-const schema = yup.object().shape({
-  name: yup.string().required('Name is required'),
-  email: yup.string().email('Invalid email format').required('Email is required'),
-  age: yup
-    .number()
-    .typeError('Age must be a number')
-    .min(18, 'You must be at least 18 years old')
-    .required('Age is required'),
-  password: yup
-    .string()
-    .min(6, 'Password must be at least 6 characters long')
-    .required('Password is required'),
-  confirm: yup
-    .string()
-    .oneOf([yup.ref('password'), null], 'Passwords must match')
-    .required('Confirm Password is required'),
-});
-
+import React from "react"
+import { useForm } from "react-hook-form"
+import * as yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup"
 function Form() {
-  // Step 2: Integrate yupResolver with react-hook-form
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(schema),
-  });
+  // schema 
+  const schema = yup.object().shape({
+    username: yup.string().required('A valid username is required'),
+    password: yup.string().required('A valid password is required'),
+  })
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+  function handle(data) {
+    console.log(data)
+  }
 
+  const { register, handleSubmit , formState : {errors} } = useForm({
+    resolver: yupResolver(schema)
+  })
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* Name field */}
-      <input type="text" placeholder="Name" {...register('name')} />
-      <p>{errors.name?.message}</p> {/* Display validation error */}
-
-      {/* Email field */}
-      <input type="email" placeholder="Email" {...register('email')} />
-      <p>{errors.email?.message}</p> {/* Display validation error */}
-
-      {/* Age field */}
-      <input type="text" placeholder="Age" {...register('age')} />
-      <p>{errors.age?.message}</p> {/* Display validation error */}
-
-      {/* Password field */}
-      <input type="password" placeholder="Password" {...register('password')} />
-      <p>{errors.password?.message}</p> {/* Display validation error */}
-
-      {/* Confirm Password field */}
-      <input type="password" placeholder="Confirm Password" {...register('confirm')} />
-      <p>{errors.confirm?.message}</p> {/* Display validation error */}
-
-      {/* Submit button */}
-      <input type="submit" />
-    </form>
-  );
+    <div>
+      <h1>Register form </h1>
+      <form onSubmit={handleSubmit(handle)}>
+        <input type="text" placeholder="username" {...register('username')} /><br></br>
+        <p style={{color : 'red'}}>{errors.username?.message}</p>
+        <input type="password" placeholder="password" {...register('password')} /><br></br>
+        <input type="password" placeholder="cpassword" {...register('cpassword')} /><br></br>
+        <input type="text" placeholder="firstname" {...register('firstname')} /><br></br>
+        <input type="text" placeholder="lastname" {...register('lastname')} /><br></br>
+        <input type="email" placeholder="email" {...register('email')} /><br></br>
+        <input type="submit" />
+      </form>
+    </div>
+  )
 }
 
-export default Form;
+export default Form
