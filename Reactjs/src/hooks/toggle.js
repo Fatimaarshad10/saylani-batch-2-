5d9aeab7d12683c6d8e2cf953721128405f755cb
Fatima Arshad <fatimaarshad118@gmail.com>
2024-09-29 07:31:28 -0700

    firebase create , delete , get all products done

--- Reactjs/src/hooks/toggle.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export function useButton() {
-  const [show, setShows] = useState(true);
-  function clickMe() {
-    show ? setShows(false) : setShows(true);
-  }
-  return {show, clickMe};
-}
