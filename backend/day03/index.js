ad22a837b528d908136478026b22344e018082a4
Fatimaarshad10 <fatimaarshad118@gmail.com>
2024-10-29 01:18:13 +0500

    day03 backend

---
+++ backend/day03/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+// // Restful api methods
+// // post ---> we can add data 
+// // get ---> we can get data
+// // put/patch --> we can update data 
+// // delete ---> we can delete data 
+
+let user = []
+app.get('/user', (req, res) => {
+  res.json(user)
+})
+app.post('/create/user', (req, res) => {
+  const userData = req.body
+  user.push(userData)
+  res.json({
+    "message": "User is created",
+    "user": userData
+  })
+})
+app.delete('/user/:id', (req, res) => {
+  const id = req.params.id
+  user = user.filter((user, id) => {user.id !== id})
+  res.json({
+    "message": "User is deleted",
+    "user": user
+  })
+})
+
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
